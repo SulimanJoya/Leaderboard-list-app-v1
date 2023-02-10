851ae4d75ad150906346f6834c0220c2fb031db3
Mohammad Suliman joya <sjoya66@gmail.com>
2023-02-10 21:11:11 +0430

    Update display function to render  data

--- src/modules/show.js
+++ src/modules/show.js
@@ -1,20 +1,11 @@
-const arr = [
-  { Name: 100 },
-  { Name: 20 },
-  { Name: 50 },
-  { Name: 78 },
-  { Name: 125 },
-  { Name: 77 },
-  { Name: 42 },
-];
-
-const display = () => {
-  const score = document.querySelector('#score-record');
-  let item = '';
-  for (let i = 0; i < arr.length; i += 1) {
-    item += `<li>Name: ${arr[i].Name}</li>`;
-  }
-  score.innerHTML = item;
+const display = async (scores) => {
+  const record = document.querySelector('#score-record');
+  record.innerHTML = '';
+  scores.forEach(({ user, score }) => {
+    record.innerHTML += `
+         <li>${user}: ${score}</li>
+          `;
+  });
 };
 
 export default display;
